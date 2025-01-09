1f95f0d5e605f0d557b8f84e5343fa1ba677c7f8
username <wecp-dummy@gmail.com>
2025-01-09 10:50:41 +0000

    Day 11

--- backend/src/main/java/com/wecp/progressive/service/impl/InsuranceServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/InsuranceServiceImpl.java
@@ -1,4 +1,3 @@
-
 package com.wecp.progressive.service.impl;
 
 import java.util.List;
@@ -31,7 +30,7 @@ public class InsuranceServiceImpl implements InsuranceService  {
     @Override
     public Insurance getInsuranceById(int insuranceId) {
         // TODO Auto-generated method stub
-        return insuranceRepository.findById(insuranceId).get();
+        return insuranceRepository.findById(insuranceId).orElse(null);
     }
 
     @Override
